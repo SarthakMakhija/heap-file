a8fb24bab04d6ff8ae0e5eef3f9b3597931b5b37
sarthak.makhija <sarthak.makhija@gmail.com>
2021-09-17 17:11:25 +0530

    Sarthak | FreePageList is now able to allocate pages

--- heap-file/FreePageList.go
+++ heap-file/FreePageList.go
@@ -14,3 +14,33 @@ func InitializeFreePageList(startingPageId uint32, pageCount int) *FreePageList 
 	}
 	return freePageList
 }
+
+func (freePageList *FreePageList) allocateAndUpdate(pages int) (bool, uint32) {
+	isAvailable, firstFreePageId, remainingFreePageIds := freePageList.allocateContiguous(pages)
+	freePageList.pageIds = remainingFreePageIds
+	return isAvailable, firstFreePageId
+}
+
+func (freePageList *FreePageList) allocateContiguous(pages int) (bool, uint32, []uint32) {
+	if len(freePageList.pageIds) < pages {
+		return false, 0, freePageList.pageIds
+	} else if pages == 1 {
+		return false, freePageList.pageIds[0], freePageList.pageIds[1:]
+	}
+
+	startingIndex, endIndex := 0, 0
+	for ; startingIndex < len(freePageList.pageIds); startingIndex++ {
+		endIndex = startingIndex + (pages - 1)
+		if endIndex < len(freePageList.pageIds) && freePageList.pageIds[endIndex] == freePageList.pageIds[startingIndex]+uint32(pages-1) {
+			break
+		}
+	}
+
+	if startingIndex >= len(freePageList.pageIds) || endIndex >= len(freePageList.pageIds) {
+		return false, 0, freePageList.pageIds
+	}
+
+	firstFreePageId := freePageList.pageIds[startingIndex]
+	freePageList.pageIds = append(freePageList.pageIds[:startingIndex], freePageList.pageIds[endIndex+1:]...)
+	return false, firstFreePageId, freePageList.pageIds
+}
--- heap-file/FreePageList_test.go
+++ heap-file/FreePageList_test.go
@@ -15,3 +15,56 @@ func TestInitializesFreePageList(t *testing.T) {
 		t.Fatalf("Expected freePageIds to be %v, received %v", expected, freePageIds)
 	}
 }
+
+func TestAllocates3Pages(t *testing.T) {
+	freePageList := InitializeFreePageList(5, 10)
+	_, startingPageId := freePageList.allocateAndUpdate(3)
+	expected := 5
+
+	if startingPageId != 5 {
+		t.Fatalf("Expected first free page id to be %v, received %v", expected, startingPageId)
+	}
+}
+
+func TestAllocates3PagesAndUpdatesFreePageList(t *testing.T) {
+	freePageList := InitializeFreePageList(5, 10)
+	freePageList.allocateAndUpdate(3)
+
+	freePageIds := freePageList.pageIds
+	expected := []uint32{8, 9, 10, 11, 12, 13, 14}
+
+	if !reflect.DeepEqual(expected, freePageIds) {
+		t.Fatalf("Expected freePageIds to be %v, received %v", expected, freePageIds)
+	}
+}
+
+func TestAllocates4Pages(t *testing.T) {
+	freePageList := InitializeFreePageList(5, 4)
+	_, startingPageId := freePageList.allocateAndUpdate(4)
+	expected := 5
+
+	if startingPageId != 5 {
+		t.Fatalf("Expected first free page id to be %v, received %v", expected, startingPageId)
+	}
+}
+
+func TestAllocates4PagesAndUpdatesFreePageList(t *testing.T) {
+	freePageList := InitializeFreePageList(5, 4)
+	freePageList.allocateAndUpdate(4)
+
+	freePageIds := freePageList.pageIds
+	expected := 0
+
+	if len(freePageIds) != 0 {
+		t.Fatalf("Expected length of freePageIds to be %v, received %v", expected, freePageIds)
+	}
+}
+
+func TestDoesNotAllocate4Pages(t *testing.T) {
+	freePageList := InitializeFreePageList(5, 3)
+	isPageAvailable, _ := freePageList.allocateAndUpdate(4)
+
+	if isPageAvailable != false {
+		t.Fatalf("Expected isPageAvailable to be false")
+	}
+}
