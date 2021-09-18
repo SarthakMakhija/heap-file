951db5786afa2751e96be93165b81f2646244e4d
sarthak.makhija <sarthak.makhija@gmail.com>
2021-09-18 16:41:34 +0530

    Sarthak | Adds an integration test to read by key

--- heap-file/Db_integration_test.go
+++ heap-file/Db_integration_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestPutsAndGets1000Tuples(t *testing.T) {
+func TestPutsAndGets1000TuplesByTupleId(t *testing.T) {
 	options := DbOptions{
 		HeapFileOptions: HeapFileOptions{
 			PageSize:                 os.Getpagesize(),
@@ -46,6 +46,59 @@ func TestPutsAndGets1000Tuples(t *testing.T) {
 		}
 
 		uint16FieldValue := readTuple.AllFields()[1].Value()
+		expectedUint16FieldValue := uint16(iterator)
+
+		if uint16FieldValue != expectedUint16FieldValue {
+			t.Fatalf("Expected field value to be %v, received %v", expectedUint16FieldValue, uint16FieldValue)
+		}
+	}
+}
+
+func TestPutsAndGets1000TuplesByKey(t *testing.T) {
+	options := DbOptions{
+		HeapFileOptions: HeapFileOptions{
+			PageSize:                 os.Getpagesize(),
+			FileName:                 "./heap.db",
+			PreAllocatedPagePoolSize: 6,
+			TupleDescriptor: tuple.TupleDescriptor{
+				FieldTypes: []field.FieldType{field.StringFieldType{}, field.StringFieldType{}, field.Uint16FieldType{}},
+			},
+		},
+		IndexOptions: index.DefaultOptions(),
+	}
+	db, _ := Open(options)
+	defer deleteFile(db.bufferPool.file)
+	defer deleteFileByName(options.IndexOptions.FileName)
+
+	tuples := make([]*tuple.Tuple, 1000)
+	for iterator := 0; iterator < 1000; iterator++ {
+		aTuple := tuple.NewTuple()
+		aTuple.AddField(field.NewStringField("Database Systems" + strconv.Itoa(iterator)))
+		aTuple.AddField(field.NewStringField("ISBN-" + strconv.Itoa(iterator)))
+		aTuple.AddField(field.NewUint16Field(uint16(iterator)))
+
+		_, _ = db.Put(aTuple)
+		tuples[iterator] = aTuple
+	}
+
+	for iterator, aTuple := range tuples {
+		readTuple, _ := db.GetByKey(aTuple.KeyField())
+
+		firstFieldValue := readTuple.AllFields()[0].Value()
+		expectedFieldValue := "Database Systems" + strconv.Itoa(iterator)
+
+		if firstFieldValue != expectedFieldValue {
+			t.Fatalf("Expected field value to be %v, received %v", expectedFieldValue, firstFieldValue)
+		}
+
+		secondFieldValue := readTuple.AllFields()[1].Value()
+		expectedFieldValue = "ISBN-" + strconv.Itoa(iterator)
+
+		if secondFieldValue != expectedFieldValue {
+			t.Fatalf("Expected field value to be %v, received %v", expectedFieldValue, secondFieldValue)
+		}
+
+		uint16FieldValue := readTuple.AllFields()[2].Value()
 		expectedUint16FieldValue := uint16(iterator)
 
 		if uint16FieldValue != expectedUint16FieldValue {
